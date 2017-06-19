f187e1deb217a215e35654d89fd1463f92f2dc9f
garfield_chou <garfield_chou@asus.com>
2017-06-19 15:42:05 +0800

    2.2.3 Regularized cost function

--- ex8/cofiCostFunc.m
+++ ex8/cofiCostFunc.m
@@ -40,7 +40,7 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
-J = sum(sum(((X * Theta' - Y).^2)(R==1))) / 2;
+J = sum(sum(((X * Theta' - Y).^2)(R==1))) / 2 + lambda * (sum(sum(Theta.^2)) + sum(sum(X.^2))) / 2;
 
 X_grad = ((X * Theta' - Y) .* R) * Theta;
 
